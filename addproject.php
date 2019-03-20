0ba33aeafdc83e2b08774e01233778b0a39d7b77
Oliver Matthews <o.matthews@rocketmail.com>
2019-03-20 13:17:25 +0000

    following changes from review

--- addproject.php
+++ addproject.php
@@ -1,3 +1,20 @@
+<?php
+$db = new PDO('mysql:host=192.168.20.20;dbname=portfolio', 'root', '');
+$db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+
+$title = $_POST ['title'];
+$url = $_POST ['url'];
+
+$sql = "INSERT INTO `portfolio`(`Title`, `Image_URL`) VALUES (:title, :url);";
+$query = $db->prepare($sql);
+$query->bindParam(':title', $title, PDO::PARAM_STR);
+$query->bindParam(':url', $url, PDO::PARAM_STR);
+$query->execute();
+
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -10,34 +27,21 @@
 </head>
 
 <body>
-    <div class="container">
+    <div class="section">
+        <div class="container">
         <form method="POST" action="addproject.php">
             <label class="label">Enter Project Title</label>
             <input type="text" name="title">
             <br>
             <br>
             <label class="label">Enter Image URL</label>
-            <input type="text" name="URL">
+            <input type="text" name="url">
             <br>
             <br>
             <input type="submit" value="Add">
         </form>
+        </div>
     </div>
 </body>
 </html>
 
-<?php
-$db = new PDO("mysql:host=192.168.20.20;dbname=portfolio", 'root', '');
-$db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-
-$title = $_POST ['title'];
-$URL = $_POST ['URL'];
-
-$sql = "INSERT INTO `portfolio`(`Title`, `Image_URL`) VALUES (:title, :URL);";
-$query = $db->prepare($sql);
-$query->bindParam(':title', $title, PDO::PARAM_STR);
-$query->bindParam(':URL', $URL, PDO::PARAM_STR);
-$query->execute();
-
-
-?>
