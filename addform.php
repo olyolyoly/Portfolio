0a71ef45977088f67c4490e47499e8732480dbdc
Oliver Matthews <o.matthews@rocketmail.com>
2019-03-21 14:18:24 +0000

    add functions

--- addform.php
+++ addform.php
@@ -2,17 +2,25 @@
 $db = new PDO('mysql:host=192.168.20.20;dbname=portfolio', 'root', '');
 $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 
-$title = $_POST ['title'];
-$url = $_POST ['url'];
+if (!empty($_POST['title']) && !empty($_POST['url'])) {
 
-$sql = "INSERT INTO `portfolio`(`Title`, `Image_URL`) VALUES (:title, :url);";
-$query = $db->prepare($sql);
-$query->bindParam(':title', $title, PDO::PARAM_STR);
-$query->bindParam(':url', $url, PDO::PARAM_STR);
-$result = $query->execute();
+    $title = $_POST ['title'];
+    $url = $_POST ['url'];
 
-if ($result) {
-    header ("location:admin.php");
-} else {
-    echo 'Database Error';
+    $sql = "INSERT INTO `portfolio`(`Title`, `Image_URL`) VALUES (:title, :url);";
+    $query = $db->prepare($sql);
+    $query->bindParam(':title', $title, PDO::PARAM_STR);
+    $query->bindParam(':url', $url, PDO::PARAM_STR);
+    $result = $query->execute();
+
+
+    if ($result) {
+        header("location:admin.php");
+    } else {
+        echo 'Database Error';
+    }
+}   else {
+    header("location:addproject.php");
 }
+
+?>
